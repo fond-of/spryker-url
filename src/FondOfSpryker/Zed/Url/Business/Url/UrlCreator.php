8221b2d1b835827d4fa2e5f792ecc79c6fc56ec4
Julian Zimmermann <julian.zimmermann@fondof.de>
2020-06-15 19:06:25 +0200

    - fix spryker bug. Re add id url for creat url after fromArray

---
+++ src/FondOfSpryker/Zed/Url/Business/Url/UrlCreator.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace FondOfSpryker\Zed\Url\Business\Url;
+
+use Spryker\Zed\Url\Business\Url\UrlCreator as SprykerUrlCreator;
+use Generated\Shared\Transfer\UrlTransfer;
+
+class UrlCreator extends SprykerUrlCreator implements UrlCreatorInterface
+{
+    /**
+     * @param \Generated\Shared\Transfer\UrlTransfer $urlTransfer
+     *
+     * @return \Generated\Shared\Transfer\UrlTransfer
+     */
+    protected function persistUrlEntity(UrlTransfer $urlTransfer)
+    {
+        $urlTransfer->requireUrl();
+        $urlEntity = $this->urlQueryContainer->queryUrl($urlTransfer->getUrl())->findOneOrCreate();
+
+        $id = $urlEntity->getIdUrl();
+        $urlEntity->fromArray($urlTransfer->modifiedToArray());
+        $urlEntity->setIdUrl($id);
+        $urlEntity->save();
+
+        $urlTransfer->fromArray($urlEntity->toArray(), true);
+
+        return $urlTransfer;
+    }
+}
---
+++ src/FondOfSpryker/Zed/Url/Business/Url/UrlCreatorInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace FondOfSpryker\Zed\Url\Business\Url;
+
+use Spryker\Zed\Url\Business\Url\UrlCreatorInterface as SprykerUrlCreatorInterface;
+
+interface UrlCreatorInterface extends SprykerUrlCreatorInterface
+{
+}
